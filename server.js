a7718e9d573485315fdd3627a1be57afd03a08b8
maira-o <mairasetecores@gmail.com>
2020-10-08 11:01:55 -0300

    add middleware

--- server.js
+++ server.js
@@ -13,8 +13,17 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+var Film = require('./app/models/film');
 
-mongoose.connect('mongodb+srv://mairao:<maira123>@cluster0.ontvx.mongodb.net/<films-flow>?retryWrites=true&w=majority');
+const uri = 'mongodb+srv://mairao:mairao@cluster0.ontvx.mongodb.net/films-flow?retryWrites=true&w=majority';
+
+MongoClient.connect(uri, { useUnifiedTopology: true }, (err, db) => {
+    assert.strictEqual(null, err);
+    // ...
+    db.close();
+});
 
 // mongoose.connect('mongodb://localhost/films-flow');
 
@@ -25,14 +34,28 @@ app.use(bodyParser.json());
 // Defining a port where the api will be executed
 var port = process.env.port || 8000;
 
+// ====================
+// *** ROTAS DA API ***
+// ====================
+
 // Creating an instance via express
 // I'll move the routers to a specific folder
 var router = express.Router();
 
-// Example of a route
-router.get('/', function(req, res) {
-res.json({ message: 'Olá, bem vinda a Film & Flow!' })
+
+// middleware
+router.use(function(req, res, next) {
+    console.log('algo está acontecendo aqui...');
+    next();
 });
+
+router.get('/', function(req, res) {
+    res.json({ message: 'Olá, vc está em Film & Flow!' })   
+});
+
+// ====================
+// *** APIs ***
+// ====================
 
 // all the routes will be prefixed with '/api'
 app.use('/api', router);
